dc54276c666562ecc93a93f706ff09151878cd23
Merl <phannakon.p@kkumail.com>
2024-09-23 01:17:09 +0700

    add prometheus dependency

--- src/main/java/com/example/GatewayService/config/CorsConfig.java
+++ src/main/java/com/example/GatewayService/config/CorsConfig.java
@@ -1,13 +1,12 @@
 package com.example.GatewayService.config;
 
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
